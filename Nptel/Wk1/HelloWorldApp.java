0d00170d3872888e1ee868a5621314794f699a1a
Ankit Podder <ankitpodder0211@gmail.com>
2020-07-27 18:59:00 +0530

    hello world wk1

---
+++ Nptel/Wk1/HelloWorldApp.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+
+class HelloWorldApp{
+	public static void main(String args[]){
+		System.out.println("Hello World1");
+		System.out.print("Hello World2\n");
+		System.out.print("Hello World2");
+	}
+}
