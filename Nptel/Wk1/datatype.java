3ebf64b5860bd341cd2de20995886ccaf2708c79
Ankit Podder <ankitpodder0211@gmail.com>
2020-07-27 19:06:08 +0530

    int

---
+++ Nptel/Wk1/datatype.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+
+class datatype{
+	public static void main(String args[]){
+		int a,b;
+		int sum;
+		a = 10;
+		b = 20;
+		sum = a + b;
+		System.out.print("Sum of a & b is := " + sum);
+	}
+}
