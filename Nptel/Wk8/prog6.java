e6b5b7b31b272d94e74dedc36e2c8a01cfc64574
Ankit Podder <ankitpodder0211@gmail.com>
2020-09-14 17:04:59 +0530

    don't complete body for abstract methods

---
+++ Nptel/Wk8/prog6.java
@@ -0,0 +1,23 @@
+import java.applet.*;
+import java.awt.*;
+public class prog6 extends Applet{
+	public void init(){ }
+	public void start(){ }
+	public void paint(Graphics g){ }
+	public void stop() { }
+	public void destroy() { }
+}
+
+/*
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Applet</title>
+	</head>
+	<body>
+		<applet code="prog6.class" width="300" height="300" codebase="."></applet>
+	</body>
+</html>
+
+*/
---
+++ Nptel/Wk8/prog7.java
@@ -0,0 +1,23 @@
+import java.applet.*;
+import java.awt.*;
+public class prog7 extends Applet{
+	// public void init(){ }
+	// public void start(){ }
+	// public void paint(Graphics g){ }
+	// public void stop() { }
+	// public void destroy() { }
+}
+
+/*
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Applet</title>
+	</head>
+	<body>
+		<applet code="prog7.class" width="600" height="300" codebase="."></applet>
+	</body>
+</html>
+
+*/
