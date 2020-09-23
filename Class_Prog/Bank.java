2ddd4f6db1ff674e765bbfb554d1364c5f2b2489
Ankit Podder <ankitpodder0211@gmail.com>
2020-09-24 00:42:27 +0530

    1st assignment done

--- Class_Prog/Bank.java
+++ Class_Prog/Bank.java
@@ -1,22 +1,33 @@
+import java.util.Scanner;
+
 class Account{
 	String name , acc_type;
 	long acc_no;
-	double interest , balance;
 
 	Account(String name , long acc_no , String acc_type){
-		this(name , acc_no , acc_type , 0);
-	}
-
-	Account(String name , long acc_no , String acc_type , double interest){
 		this.name     = name;
 		this.acc_no   = acc_no;
 		this.acc_type = acc_type;
-		this.interest = interest;
-		this.balance  = 0;
 	}
 
-	void balance(){
-		System.out.println("Your current balance is : "+this.balance);
+	void display_balance(double amount){
+		System.out.printf("Your current balance is : %.2f\n",amount);
+	}
+
+	void details(){
+		System.out.printf("User details:\n===========================\nName : %s\nAccount no : %d\nAccount type : %s\n",this.name,this.acc_no,this.acc_type);
+	}
+}
+
+class Savings extends Account{
+	double penalty , min_balance , balance , interest;
+	
+	Savings(String name , long acc_no , String acc_type , double interest , double penalty , double min_amount){
+		super(name , acc_no , acc_type);
+		this.interest    = interest;
+		this.penalty     = penalty;
+		this.min_balance = min_amount;
+		this.balance     = 0;
 	}
 
 	void deposit(double amount){
@@ -29,115 +40,173 @@ class Account{
 		System.out.println("Interest is added!");
 	}
 
-	void details(){
-		System.out.printf("User details:\n===========================\nName : %s\nAccount no : %d\nAccount type : %s\nIneterest Rate : %.2f %%\n",this.name,this.acc_no,this.acc_type,this.interest);
-	}
-}
+	void withdrawal(double amount){
+		if(this.have_min_balance() == false){
+			System.out.println("You don't have enough balance to withdrawal!");
+			return;
+		}
+		if(this.balance - amount < 0){
+			System.out.println("You don't have enough balance to withdrawal!");
+			return;
+		}
 
-class Savings extends Account{
-	double penalty , min_balance;
-	Savings(String name , long acc_no , String acc_type ){
-		this(name , acc_no , acc_type , 10 , 100 , 1000);
-	}
-
-	Savings(String name , long acc_no , String acc_type , double interest){
-		this(name , acc_no , acc_type , interest , 100 , 1000);
-	}
-	Savings(String name , long acc_no , String acc_type , double interest , double penalty , double min_amount){
-		super(name , acc_no , acc_type , interest);
-		this.penalty     = penalty;
-		this.min_balance = min_amount;
+		this.balance -= amount;
+		System.out.println("withdrawal successful!");
 	}
 
 	boolean have_min_balance(){
-		if(super.balance < this.min_balance){
-			//System.out.println("You don't have enough balance , please deposit money!");
+		if(this.balance < this.min_balance){
 			return false;
 		}
 		return true;
 	}
 
-	void withdrawl(double amount){
-		if(this.have_min_balance() == false){
-			System.out.println("You don't have enough balance to withdrawl!");
-			return;
-		}
-		if(super.balance - amount < 0){
-			System.out.println("You don't have enough balance to withdrawl!");
-			return;
-		}
-
-		super.balance -= amount;
-		System.out.println("Withdrawl successful!");
-	}
-
 	void check_min_balance(){
 		if(this.have_min_balance() == false){
 			System.out.println("You don't have enough balance , please deposit money!");
-			super.balance -= this.penalty;
+			this.balance -= this.penalty;
 		}
 		else{
 			System.out.println("You have enough balance , don't worry !");
 		}
 	}
+
+	void display_balance(){
+		super.display_balance(this.balance);
+	}
+
 	void details(){
 		super.details();
-		System.out.printf("Min amount : %.2f\nPenalty : %.2f\nBalance : %.2f\n\n",this.min_balance , this.penalty , super.balance);
+		System.out.printf("Ineterest Rate : %.2f %%\nMin amount : %.2f\nPenalty : %.2f\nBalance : %.2f\n\n",this.interest , this.min_balance , this.penalty , this.balance);
 	}
 }
 
 class Current extends Account{
-	Current(String name , long acc_no , String acc_type ){
-		this(name , acc_no , acc_type , 2);
-	}
-
+	double interest , balance;
 	Current(String name , long acc_no , String acc_type , double interest){
-		super(name , acc_no , acc_type , interest);
-	}
-	void details(){
-		super.details();
-		System.out.printf("Current Balance : %.2f\n\n",super.balance);
+		super(name , acc_no , acc_type);
+		this.interest = interest;
+		this.balance  = 0;
 	}
 
-	void withdrawl(double amount){
-		if(super.balance - amount < 0){
-			System.out.println("You don't have enough balance to withdrawl!");
+	void deposit(double amount){
+		this.balance += amount;
+		System.out.println("Deposit is done successfully !");
+	}
+
+	void withdrawal(double amount){
+		if(this.balance - amount < 0){
+			System.out.println("You don't have enough balance to withdrawal!");
 			return;
 		}
 
-		super.balance -= amount;
-		System.out.println("Withdrawl successful!");
+		this.balance -= amount;
+		System.out.println("withdrawal successful!");
 	}
+
+	void addInterest(){
+		this.balance = this.balance + (this.balance * (this.interest / 100));
+		System.out.println("Interest is added!");
+	}
+
+	void details(){
+		super.details();
+		System.out.printf("Ineterest Rate : %.2f %%\nCurrent Balance : %.2f\n\n",this.interest , this.balance);
+	}
+
+	void display_balance(){
+		super.display_balance(this.balance);
+	}
+
 }
 
-class Bank{
+public class Bank{
 	public static void main(String[] args) {
-		Savings a1 = new Savings("Ankit" , 58792 , "Savings");
-		a1.details();
-
-		a1.deposit(15000);
-		a1.withdrawl(50000);
-		a1.withdrawl(10000);
-		a1.balance();
-		a1.addInterest();
-		a1.balance();
-		a1.check_min_balance();
-		a1.withdrawl(4800);
-		a1.balance();
-		a1.check_min_balance();
-		a1.balance();
-
-		System.out.println("\n******************************************************************\n\n");
-
-		Current a2 = new Current("Anirban" , 45892 , "Current" , 3.5);
-		a2.details();
-
-		a2.deposit(100000);
-		a2.withdrawl(200000);
-		a2.balance();
-		a2.addInterest();
-		a2.balance();
-		a2.withdrawl(50000);
-		a2.balance();
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter 1 -> if you want to create Savings acc \nEnter 2 -> if you want to create Current acc\nEnter your choice : ");
+		int ch = sc.nextInt();
+		double interest;
+		System.out.print("Name : ");
+		sc.nextLine();
+		String name = sc.nextLine();
+		switch(ch){
+			case 1:
+				double  penalty , min_amount;
+				System.out.print("Enter interest , penalty , min_amount : ");
+				interest   = sc.nextDouble();
+				penalty    = sc.nextDouble();
+				min_amount = sc.nextDouble();
+				Savings acc1 = new Savings(name , 15423 , "Savings",	interest , penalty , min_amount);
+				System.out.print("Enter 1->Deposit\nEnter 2->withdrawal\nEnter 3-> display_balance \nEnter 4 -> check_min_balance\nEnter 5 -> Show details\nEnter 6-> Add Interest\nEnter 7-> Quit\n");
+				int op;
+				do{
+					System.out.print("Enter the option : ");
+					op = sc.nextInt();
+					switch(op){
+						case 1:
+							System.out.print("Enter amount to deposit : ");
+							double a1 = sc.nextDouble();
+							acc1.deposit(a1);
+							break;
+						case 2:
+							System.out.print("Enter amount to withdrawal : ");
+							double a2 = sc.nextDouble();
+							acc1.withdrawal(a2);
+							break;
+						case 3:
+							acc1.display_balance();
+							break;
+						case 4:
+							acc1.check_min_balance();
+							break;
+						case 5:
+							acc1.details();
+							break;
+						case 6:
+							acc1.addInterest();
+							break;
+						case 7:
+							break;
+						default:
+							System.out.println("Enter the proper operation num.");
+							break;
+					}
+				}while(op != 7);
+			case 2:
+				System.out.print("Enter interest : ");
+				interest   = sc.nextDouble();
+				Current acc2 = new Current(name , 15423 , "Current",	interest);
+				System.out.print("Enter 1->Deposit\nEnter 2->withdrawal\nEnter 3-> display_balance\nEnter 4 -> Show details\nEnter 5-> Add Interest\nEnter 6-> Quit\n");
+				do{
+					System.out.print("Enter the option : ");
+					op = sc.nextInt();
+					switch(op){
+						case 1:
+							System.out.print("Enter amount to deposit : ");
+							double a1 = sc.nextDouble();
+							acc2.deposit(a1);
+							break;
+						case 2:
+							System.out.print("Enter amount to withdrawal : ");
+							double a2 = sc.nextDouble();
+							acc2.withdrawal(a2);
+							break;
+						case 3:
+							acc2.display_balance();
+							break;
+						case 4:
+							acc2.details();
+							break;
+						case 5:
+							acc2.addInterest();
+							break;
+						case 6:
+							break;
+						default:
+							System.out.println("Enter the proper operation num.");
+							break;
+					}
+				}while(op != 6);
+		}
 	}
 }
